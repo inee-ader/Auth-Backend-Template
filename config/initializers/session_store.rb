7b0b058b740594bb734cd4ec87669198a2573b46
xoliio <xoliio@gmail.com>
2021-01-10 13:49:36 -0800

    completes session store initializer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
-Rails.application.config.session_store :cookie_store, key:'_auth_app', domain: 'http://localhost:3000'
-# localhost is placeholder. use whatever domain I bought here. 
+if Rails.env == 'production'
+    Rails.application.config.session_store :cookie_store, key:'_auth_app', domain: 'http://localhost:3000'
+    # localhost is placeholder. use whatever domain I bought here. 
+else
+    Rails.application.config.session_store :cookie_store, key:'_auth_app'
+end
+
 
